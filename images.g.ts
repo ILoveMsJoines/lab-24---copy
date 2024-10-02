c8f4400efcc41d12ec565dfa688e83cda5d0f64c
ILoveMsJoines <b.mitchell@mcaabhm.org>
2024-10-02 12:51:58 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Gnarp Gnarp":return img`
+. . . . . . 7 . . . 3 . 3 . . . 
+. . . . . . . 7 3 . . 3 . . . . 
+. . . . . . . 3 7 7 7 f 1 f 3 7 
+. . . . . . . f 1 f 7 f f f 7 . 
+. . . . . . . f f f 7 1 f f 3 . 
+. . . . . . . 1 f f 7 7 7 7 . . 
+. . . . . . . . 7 7 7 7 7 7 . . 
+. . . . . . . . . . 7 7 f f . . 
+. . . . . . . . 7 . . f f 2 . . 
+. . . . . . . . 7 8 7 8 . . . . 
+. . . . . . . . . 8 8 8 . 7 7 . 
+. . . . . . . 8 8 8 5 8 7 7 . . 
+. . . . . . . 8 8 8 8 8 . . . . 
+. . . . . . . 8 8 8 8 8 . . . . 
+. . . . . . . . 8 8 8 8 8 . . . 
+. . . . . . . . . 8 . . 8 . . . 
+`;
+            case "image2":
+            case "evil":return img`
+. . . . . . a a a . . . . . . . 
+. . . . a a a a a a a . . 3 . . 
+. . a f a f a a a a a a 3 3 . . 
+1 1 1 f f f f a a a a a a 3 3 . 
+1 1 1 f f f a a a a a a a . 3 3 
+1 1 1 f f f f a a a a a a a 3 . 
+a f f f f f a a a a a a a a . . 
+a f f f 1 f a a a a a f a f a . 
+a a a a a a a a 1 1 1 f f f f a 
+a a a a a a a a 1 1 1 f f f a a 
+a a a a a a a a 1 1 1 f f f f a 
+a a a f f f a a a f f f f f a a 
+. . a f f f a a a f f f 1 f a a 
+. . a f f 3 a a a a a a a a a a 
+. . a a a a a a a a a a a a . . 
+. . . a a a a a a a a a a . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
